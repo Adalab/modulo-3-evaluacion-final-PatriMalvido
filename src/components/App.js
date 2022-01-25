0b7c9462946bf650d1330c9d88f4f55a29afd8f7
Patri <patri@malvidopazo.es>
2022-01-25 15:54:15 +0100

    add html

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,41 @@
-
 import '../styles/App.scss';
 
 function App() {
   return (
-    <div >
-      <h1>Hola mundo</h1>
+    <div>
+      <main>
+        <h1 className="page__title">Harry Potter</h1>
+        <section className="section__form">
+          <form className="form">
+            <label className="form__label" htmlFor="name">
+              Busca por personaje:
+            </label>
+            <input
+              className="form__input-text"
+              type="text"
+              name="name"
+              id="text"
+            />
+            <label className="form__label" htmlFor="house">
+              Seleccciona la casa:
+            </label>
+            <select className="form__input-text" name="house" id="house">
+              <option value="gryffindor">Gryffindor</option>
+              <option value="hufflepuff">Hufflepuff</option>
+              <option value="ravenclaw">Ravenclaw</option>
+              <option value="slytherin">Slytherin</option>
+            </select>
+          </form>
+        </section>
+        <section className="section_cards">
+          <ul className="cards">
+            <li className="card">aqui va la info del personaje</li>
+          </ul>
+        </section>
+      </main>
+      <footer className="footer">
+        <p>texto para el footer</p>
+      </footer>
     </div>
   );
 }
