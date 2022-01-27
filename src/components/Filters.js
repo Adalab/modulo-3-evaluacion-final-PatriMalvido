fa2dcd4ced6130af7211e58141249ae738dcef42
Patri <patri@malvidopazo.es>
2022-01-27 10:28:32 +0100

    create gitignore

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,18 +1,15 @@
 import FilterByName from './FiltersByName';
 import FilterByHouse from './FilterByHouse';
 
-const Filters = (props)=> {
-return (
+const Filters = (props) => {
+  return (
     <section className="section__form">
-          <form className="form" onSubmit={(ev)=>ev.preventDefault()}>
-              <FilterByName handleFilter={props.handleFilter}/>
-
-            <FilterByHouse handleFilter={props.handleFilter}/>
-
-
-          </form>
-        </section>
-)
-}
+      <form className="form" onSubmit={(ev) => ev.preventDefault()}>
+        <FilterByName handleFilter={props.handleFilter} />
+        <FilterByHouse handleFilter={props.handleFilter} />
+      </form>
+    </section>
+  );
+};
 
 export default Filters;
